b37cacb9759b08ffb908f55ac4c44ca4f2d6a81f
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-07 20:57:18 -0600

    Pagination with pagy added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,5 @@ module ApplicationHelper
         gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow mt-3 mx-auto d-block")
     end
+    include Pagy::Frontend
 end
